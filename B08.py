9726f9be5ab61cba86ac89646d114eaec0a6ac07
bigmouse08 <114320063+bigmouse08@users.noreply.github.com>
2022-09-25 10:36:41 +0700

    Add files via upload

---
+++ B08.py
@@ -0,0 +1,11 @@
+import random
+import sys
+import time
+def mengetik(s):
+   for c in s +'\n':
+	sys.stdout. write(c)
+	sys.stdout.flush()
+
+	time.sleep(random.random( )* 0.3)
+
+mengetik('selamat Datang, MR BIG MOUSE\nhello MR.BIG MOUSE\napa kabar?.')
